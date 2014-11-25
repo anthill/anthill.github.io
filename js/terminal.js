330c27b3d12d8835668d96f7fa904dd4a603fcfc
Romain Crestey <rcrestey@gmail.com>
2014-11-25 18:17:28 +0100

    prevent Default

--- js/terminal.js
+++ js/terminal.js
@@ -1,14 +1,39 @@
 'use strict';
-
+// var $ = require('jQuery');
 
 module.exports = function(){
 
 	var terminal = document.getElementById('terminal');
 
+	function preventEscape(event){
+		if (event.keyCode === 8)
+			event.preventDefault();
+	}
+
 	function keyHandler(event){
-		var key = event.keyCode;
-		// console.log('key ', String.fromCharCode(parseInt(key)));
-		event.target.innerHTML += String.fromCharCode(parseInt(key));
+		var key = parseInt(event.keyCode);
+
+		console.log('key pressed ', key);
+
+		if( key === 8 ){ // 8 == backspace
+			var text = event.target.innerHTML;
+			console.log('text ', text.splice(text.length, -1));
+			event.target.innerHTML = text.splice(text.length, -1);
+		}
+
+		if( key === 13 ){ // 13 == return
+			event.preventDefault();
+			var text = event.target.innerHTML;
+			console.log('text ', text.splice(text.length, -1));
+			event.target.innerHTML = text.splice(text.length, -1);
+		}
+		
+		if( key === 32 ) // 32 == spacebar
+			event.preventDefault();
+		// if( key === 32 ) // 32 == spacebar
+		// 	event.preventDefault();
+
+		event.target.innerHTML += String.fromCharCode(key);
 	}
 
 	function activateTerminal(event){
@@ -16,6 +41,9 @@ module.exports = function(){
 		event.target.style.color = 'white';
 		event.target.focus();
 		event.target.addEventListener('keypress', keyHandler);
+		window.addEventListener('keydown', preventEscape);
+
+		// $(document).bind("keypress", escapeBehaviour);
 	}
 
 	function deactivateTerminal(event){
@@ -23,6 +51,9 @@ module.exports = function(){
 		event.target.style.color = 'black';
 		event.target.blur();
 		event.target.removeEventListener('keypress', keyHandler);
+		window.removeEventListener('keydown', preventEscape);
+
+		// $(document).unbind("keypress", escapeBehaviour);
 	}
 
 	terminal.addEventListener('mouseover', activateTerminal);
