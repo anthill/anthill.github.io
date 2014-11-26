d7da9ced5115c67a7195f17b54fd65d7e9aec388
Romain Crestey <rcrestey@gmail.com>
2014-11-26 16:00:35 +0100

    bug fix: click on tab

--- js/index.js
+++ js/index.js
@@ -11,27 +11,50 @@ else{
 	fallback.removeAttribute('hidden');
 }
 
+function activatePage(){
+	tabs[0].className = 'tab'; 
+	tabs[1].className = 'tab'; 
+	tabs[2].className = 'tab'; 
+	tabs[3].className = 'tab'; 
+	tabs[4].className = 'tab'; 
+
+	event.target.classname += ' active';
+}
+
 function findPage() {
 	var currentY = window.scrollY;
 
 	var ratios = pages.map(function(page, i){
-		if (currentY - page.bottom > 0 || currentY + window.innerHeight < page.top)
+		if (currentY - page.bottom >= 0 || currentY + window.innerHeight < page.top)
 			return 0;
-		else if (currentY < page.bottom && currentY > page.top)
+		else if (currentY < page.bottom && currentY > page.top) // currentY is in the middle of page
 			return Math.min((page.bottom - currentY)/window.innerHeight, 1);
-		else
+		else { // currentY is just before page
 			return 1 - (page.top - currentY) / window.innerHeight;
+		}
+			
 	});
+
+	console.log('Ratios :', ratios);
 
 	var i = ratios.indexOf(Math.max.apply(Math, ratios)) - 1;
 
-	if (i >= 0 && menu[i].className != 'tab active') {
-		menu[0].className = 'tab'; 
-		menu[1].className = 'tab'; 
-		menu[2].className = 'tab'; 
-		menu[3].className = 'tab'; 
-		menu[4].className = 'tab'; 
-		menu[i].className += ' active'; 
+	console.log('i :', i);
+
+	if (i === -1){
+		tabs[0].className = 'tab'; 
+		tabs[1].className = 'tab'; 
+		tabs[2].className = 'tab'; 
+		tabs[3].className = 'tab'; 
+		tabs[4].className = 'tab'; 
+	}
+	else if (i >= 0 && tabs[i].className != 'tab active') {
+		tabs[0].className = 'tab'; 
+		tabs[1].className = 'tab'; 
+		tabs[2].className = 'tab'; 
+		tabs[3].className = 'tab'; 
+		tabs[4].className = 'tab'; 
+		tabs[i].className += ' active'; 
 	}
 }
 
@@ -49,7 +72,7 @@ var projectsRect = projects.getBoundingClientRect();
 var teamRect = team.getBoundingClientRect();
 
 var rects = [headerRect, aboutRect, spiritRect, projectsRect, teamRect];
-var menu = document.getElementsByClassName('tab');
+var tabs = document.getElementsByClassName('tab');
 
 var pages = rects.map(function(rect){
 	return {
@@ -61,4 +84,9 @@ var pages = rects.map(function(rect){
 
 
 window.addEventListener("scroll", findPage);
+// tabs[0].addEventListener("click", activatePage);
+// tabs[1].addEventListener("click", activatePage);
+// tabs[2].addEventListener("click", activatePage);
+// tabs[3].addEventListener("click", activatePage);
+// tabs[4].addEventListener("click", activatePage);
 
