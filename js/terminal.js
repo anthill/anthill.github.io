88865a557224eeed131e14ff3bd056f59b630a0e
Romain Crestey <rcrestey@gmail.com>
2014-12-01 17:17:33 +0100

    input analysis

--- js/terminal.js
+++ js/terminal.js
@@ -9,6 +9,11 @@ module.exports = function(){
 	var baseTextLength = baseText.length;
 	var cursorLength = cursor.length;
 
+	var helpText = "<br/>Hereunder are listed the main functions you can use in this terminal:<br/>'help': display help message<br/>'romain': describe who is romain";
+
+	var history = [];
+	var current = 0;
+
 	function preventEscape(event){
 
 		var key = event.keyCode;
@@ -28,14 +33,16 @@ module.exports = function(){
 				break;
 			case 38: // up
 				event.preventDefault();
-				terminal.scrollTop -= 19; // 19 should be the actual computed font-size
+				keyHandler(event);
+				// terminal.scrollTop -= 19; // 19 should be the actual computed font-size
 				break;
 			case 39: // right
 				event.preventDefault();
 				break;
 			case 40: // bottom
 				event.preventDefault();
-				terminal.scrollTop += 19; // 19 should be the actual computed font-size
+				keyHandler(event);
+				// terminal.scrollTop += 19; // 19 should be the actual computed font-size
 				break;
 
 			default:
@@ -44,8 +51,76 @@ module.exports = function(){
 		
 	}
 
-	function terminalSays(text){
-		return '<br/>' + text;
+	function checkLength(array, nb){
+		return (array.length != nb);
+	}
+
+	function checkInt(nb, value1, value2){
+		return (!isNaN(parseInt(nb)) && nb === parseInt(nb) && (nb >= value1) && (nb <= value2));
+	}
+
+	function checkFloat(nb, value1, value2){
+		nb = parseFloat(nb);
+		return ((!isNaN(nb)) && (nb >= value1) && (nb <= value2));
+	}
+
+	function inputAnalysis(string){
+		var input = string.split(' ');
+
+		switch (input[0]){
+
+			case 'speed':
+				if (!checkLength(input, 1))
+					return '<br/>This function needs ' + 1 + ' argument';
+				if (!checkFloat(input[1], 1.0, 10.0))
+					return '<br/>This function needs a float between ' + 1.0 + ' and ' + 10.0;
+				
+				// setVariables(input[1], 0, 0, 0, 0, 0);
+				return '<br/>OK';
+
+			case 'nbAnts':
+				if (!checkLength(input, 1))
+					return '<br/>This function needs ' + 1 + ' argument';
+				if (!checkInt(input[1], 1, 4000))
+					return '<br/>This function needs an integer between ' + 1 + ' and ' + 4000;
+				
+				// setVariables(0, input[1], 0, 0, 0, 0);
+				return '<br/>OK';
+
+			case 'nbStarts':
+				break;
+
+			case 'nbRand':
+				break;
+
+			case 'repSize':
+				break;
+
+			case 'repSpeed':
+				break;
+			
+			case 'weight':
+				break;
+
+			case 'set':
+				break;
+
+			case 'help':
+				return helpText;
+				break;
+
+			case 'romain':
+				return "<br/>That's a great guy !" ;
+				break;
+
+			case 'defaults':
+				return defaultText;
+				break;
+
+			default:
+				return "<br/>I don't understand... Type 'help' for infos";
+				break;
+		}
 	}
 
 	function keyHandler(event){
@@ -58,7 +133,7 @@ module.exports = function(){
 
 		switch (key){
 			case 8: // backspace
-				var text = event.target.innerHTML.slice(0, -cursorLength);
+				var text = event.target.innerHTML.slice(0, - cursorLength);
 				event.target.innerHTML = text.slice(0, -1) + cursor;
 				// console.log('cursor :', cursor);
 				// console.log('baseText :', baseText);
@@ -69,16 +144,45 @@ module.exports = function(){
 
 				var previousContent = content.slice(0, length);
 
-				var input = content.slice(baseTextLength+1, length);
+				var input = content.slice(baseTextLength+1, length); // hit 'enter' seems to add 1 to string
+				
+				history.push(input);
+				current = history.length;
 				// console.log('raw :', content.slice(baseTextLength+1, length));
 				// console.log('Rcontent :', content + baseText);
 
 				// Check for keywords and launch appropriate function
-				var answer = terminalSays('Vous avez entré : ' + input, event.target);
+				var answer = inputAnalysis(input);
 				
 				event.target.innerHTML = previousContent + answer + baseText + cursor;
-				baseTextLength = previousContent.length + answer.length + baseText.length -3; // MAIS POURQUOI -3 ???????
+				baseTextLength = previousContent.length + answer.length + baseText.length - 3; // -3 is a hack used to compensate count malfunction
 				terminal.scrollTop = terminal.scrollHeight;
+				break;
+			case 38: // up
+				var content = event.target.innerHTML;
+				var previousContent = content.slice(0, baseTextLength + 1);
+
+				current -= 1;
+
+				if (current < 0)
+					current = 0;
+
+				event.target.innerHTML = previousContent + history[current] + cursor;
+				break;
+
+			case 40: // down
+				var content = event.target.innerHTML;
+				var previousContent = content.slice(0, baseTextLength + 1);
+
+				current += 1;
+
+				console.log('current ', current);
+				console.log('history length ', history.length);
+
+				if (current >= history.length)
+					current = history.length - 1;
+
+				event.target.innerHTML = previousContent + history[current] + cursor;
 				break;
 			
 			default:
