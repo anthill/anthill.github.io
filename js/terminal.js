b7dee386b204322f0ac3150e0dc4a6b3392b5b68
Romain Crestey <rcrestey@gmail.com>
2014-12-04 17:27:49 +0100

    terminal with div

--- js/terminal.js
+++ js/terminal.js
@@ -1,12 +1,30 @@
 'use strict';
+var _antColony = require('AntColony');
 
-module.exports = function(){
+module.exports = function(container){
+
+	var defaultOptions = {
+		velocity: 0.001,
+		nbAnts: 4000,
+		weight: 10,
+		repSize: 0.05,
+		repSpeed: 0.002,
+		nbStart: 20,
+		nbRand: 500
+		// obj par defaut
+	};
+
+	var options = defaultOptions;
+
+	// launch animation
+	var antColony = _antColony(container, defaultOptions);
+
 
 	var terminal = document.getElementById('terminal');
 
-	var baseText = '<br/><span class="prompt">ants&gt;&nbsp;</span>';
+	var baseText = '<span class="prompt">ants&gt;&nbsp;</span>';
 	var cursor = '<span class="cursor">&nbsp;</span>';
-	var baseTextLength = baseText.length;
+	// var baseTextLength = baseText.length; // 5 is for the <br/>
 	var cursorLength = cursor.length;
 
 	var helpText = "<br/>Hereunder are listed the main functions you can use in this terminal:<br/>'help': display help message<br/>'romain': describe who is romain";
@@ -57,6 +75,10 @@ module.exports = function(){
 	}
 
 	function checkInt(nb, value1, value2){
+		console.log("1 :", !isNaN(parseInt(nb)));
+		console.log("2 :", nb === parseInt(nb));
+		console.log("3 :", (nb >= value1));
+		console.log("4 :", (nb <= value2));
 		return (!isNaN(parseInt(nb)) && nb === parseInt(nb) && (nb >= value1) && (nb <= value2));
 	}
 
@@ -73,34 +95,95 @@ module.exports = function(){
 			case 'speed':
 				if (!checkLength(input, 1))
 					return '<br/>This function needs ' + 1 + ' argument';
-				if (!checkFloat(input[1], 1.0, 10.0))
-					return '<br/>This function needs a float between ' + 1.0 + ' and ' + 10.0;
-				
-				// setVariables(input[1], 0, 0, 0, 0, 0);
-				return '<br/>OK';
+				if (!checkFloat(input[1], 0.0005, 0.005))
+					return '<br/>This function needs a float between ' + 0.0005 + ' and ' + 0.005;
+
+				Object.assign(options, {velocity: parseFloat(input[1])});
+				options.nbStart = 0;
+				options.nbRand = undefined;
+				console.log("options :", options);
+				antColony.changeOptions(options);
+
+				break;
 
 			case 'nbAnts':
 				if (!checkLength(input, 1))
-					return '<br/>This function needs ' + 1 + ' argument';
-				if (!checkInt(input[1], 1, 4000))
-					return '<br/>This function needs an integer between ' + 1 + ' and ' + 4000;
-				
-				// setVariables(0, input[1], 0, 0, 0, 0);
-				return '<br/>OK';
+					return 'This function needs ' + 1 + ' argument';
+				if (!checkInt(parseFloat(input[1]), 1, 10000))
+					return 'This function needs an integer between ' + 1 + ' and ' + 10000;
 
-			case 'nbStarts':
+				Object.assign(options, {nbAnts: parseInt(input[1])});
+				options.nbStart = 0;
+				options.nbRand = undefined;
+				console.log("options :", options);
+				antColony.changeOptions(options);
+
+				break;
+
+			case 'nbStart':
+				if (!checkLength(input, 1))
+					return 'This function needs ' + 1 + ' argument';
+				if (!checkInt(parseFloat(input[1]), 1, 500))
+					return 'This function needs an integer between ' + 1 + ' and ' + 500;
+
+				Object.assign(options, {nbStart: parseInt(input[1])});
+				console.log("options :", options);
+				antColony.changeOptions(options);
+
 				break;
 
 			case 'nbRand':
+				if (!checkLength(input, 1))
+					return 'This function needs ' + 1 + ' argument';
+				if (!checkInt(parseFloat(input[1]), 0, 10000))
+					return 'This function needs an integer between ' + 0 + ' and ' + 10000;
+
+				Object.assign(options, {nbRand: parseInt(input[1])});
+				console.log("options :", options);
+				antColony.changeOptions(options);
+
 				break;
 
 			case 'repSize':
+				if (!checkLength(input, 1))
+					return 'This function needs ' + 1 + ' argument';
+				if (!checkFloat(input[1], 0.01, 0.1))
+					return 'This function needs an integer between ' + 0.01 + ' and ' + 0.1;
+
+				Object.assign(options, {repSize: parseFloat(input[1])});
+				options.nbStart = 0;
+				options.nbRand = undefined;
+				console.log("options :", options);
+				antColony.changeOptions(options);
+
 				break;
 
 			case 'repSpeed':
+				if (!checkLength(input, 1))
+					return 'This function needs ' + 1 + ' argument';
+				if (!checkFloat(input[1], 0.001, 0.01))
+					return 'This function needs an integer between ' + 0.001 + ' and ' + 0.01;
+
+				Object.assign(options, {repSpeed: parseFloat(input[1])});
+				options.nbStart = 0;
+				options.nbRand = undefined;
+				console.log("options :", options);
+				antColony.changeOptions(options);
+
 				break;
 			
 			case 'weight':
+				if (!checkLength(input, 1))
+					return 'This function needs ' + 1 + ' argument';
+				if (!checkInt(parseFloat(input[1]), 1, 1000))
+					return 'This function needs an integer between ' + 1 + ' and ' + 1000;
+
+				Object.assign(options, {repSpeed: parseInt(input[1])});
+				options.nbStart = 0;
+				options.nbRand = undefined;
+				console.log("options :", options);
+				antColony.changeOptions(options);
+
 				break;
 
 			case 'set':
@@ -111,7 +194,7 @@ module.exports = function(){
 				break;
 
 			case 'romain':
-				return "<br/>That's a great guy !" ;
+				return "That's a great guy !" ;
 				break;
 
 			case 'defaults':
@@ -119,7 +202,7 @@ module.exports = function(){
 				break;
 
 			default:
-				return "<br/>I don't understand... Type 'help' for infos";
+				return "I don't understand... Type 'help' for infos";
 				break;
 		}
 	}
@@ -130,100 +213,120 @@ module.exports = function(){
 					((event.charCode) ? event.charCode : 
 						((event.keyCode) ? event.keyCode : 0));
 
-		console.log('key pressed ', key);
+		// console.log('key pressed ', key);
+
+		var line = document.getElementById('currentLine');
 
 		switch (key){
 			case 8: // backspace
 				if (letterCount > 0) { // check if there are letters to delete
-					var text = event.target.innerHTML.slice(0, - cursorLength);
-					event.target.innerHTML = text.slice(0, -1) + cursor;
+					var text = line.innerHTML.slice(0, - cursorLength);
+					line.innerHTML = text.slice(0, -1) + cursor;
 
 					letterCount --;
-					console.log('letter Count :', letterCount);
+					// console.log('letter Count :', letterCount);
 					// console.log('cursor :', cursor);
 					// console.log('baseText :', baseText);
 				}
 				
 				break;
 			case 13: // return
-				var content = event.target.innerHTML;
+				var content = line.innerHTML;
 				var length = content.length - cursorLength;
 
-				var previousContent = content.slice(0, length);
+				line.innerHTML = content.slice(0, length);
 
-				var input = content.slice(baseTextLength+1, length); // hit 'enter' seems to add 1 to string
+				// var previousContent = content.slice(0, length);
+
+				var input = content.slice(baseText.length, length); // hit 'enter' seems to add 1 to string
+				console.log('input :', input);
 				
 				history.push(input);
 				currentLine = history.length;
 				letterCount = 0;
-				console.log('letter Count :', letterCount);
+				// console.log('letter Count :', letterCount);
 				// console.log('raw :', content.slice(baseTextLength+1, length));
 				// console.log('Rcontent :', content + baseText);
 
 				// Check for keywords and launch appropriate function
 				var answer = inputAnalysis(input);
 				
-				event.target.innerHTML = previousContent + answer + baseText + cursor;
-				baseTextLength = previousContent.length + answer.length + baseText.length - 3; // -3 is a hack used to compensate count malfunction
+				var newLine = document.createElement('div');
+				newLine.id = 'currentLine';
+				newLine.innerHTML = baseText + cursor;
+				line.id = '';
+			
+				if (answer){
+					var answerLine = document.createElement('div');
+					answerLine.innerHTML = answer;
+					terminal.appendChild(answerLine);
+				}
+
+				terminal.appendChild(newLine);
+				// console.log('newline :', newLine.innerHTML);
 				terminal.scrollTop = terminal.scrollHeight;
+
 				break;
 			case 38: // up
-				var content = event.target.innerHTML;
-				var previousContent = content.slice(0, baseTextLength + 1);
+				var content = line.innerHTML;
 
 				currentLine -= 1;
 
 				if (currentLine < 0)
 					currentLine = 0;
 
-				event.target.innerHTML = previousContent + history[currentLine] + cursor;
+				letterCount = history[currentLine].length;
+				// console.log('letter Count :', letterCount);
+
+				line.innerHTML = baseText + history[currentLine] + cursor;
 				break;
 
 			case 40: // down
-				var content = event.target.innerHTML;
-				var previousContent = content.slice(0, baseTextLength + 1);
+				var content = line.innerHTML;
 
 				currentLine += 1;
 
-				console.log('current ', currentLine);
-				console.log('history length ', history.length);
+				// console.log('current ', currentLine);
+				// console.log('history length ', history.length);
 
 				if (currentLine >= history.length)
 					currentLine = history.length - 1;
 
-				event.target.innerHTML = previousContent + history[currentLine] + cursor;
+				letterCount = history[currentLine].length;
+				// console.log('letter Count :', letterCount);
+
+				line.innerHTML = baseText + history[currentLine] + cursor;
 				break;
 			
 			default:
-				var content = event.target.innerHTML;
+				var content = line.innerHTML;
 				var length = content.length - cursorLength;
 				// console.log('content :', content);
 				var content = content.slice(0, length);
 				// console.log('cursor :', cursor);
 				// console.log('content :', content);
-				letterCount ++; // letter count
-				console.log('letter Count :', letterCount);
+				letterCount ++;
 
-				event.target.innerHTML = content + String.fromCharCode(key) + cursor;
+				line.innerHTML = content + String.fromCharCode(key) + cursor;
 
 				break;
 		}
 	}
 
 	function activateTerminal(event){
-		event.target.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-		event.target.style.color = 'white';
 		event.target.focus();
+		
 		event.target.addEventListener('keypress', keyHandler);
 		window.addEventListener('keydown', preventEscape);
+		event.stopPropagation();
 	}
 
 	function deactivateTerminal(event){
-		event.target.style.backgroundColor = 'rgba(255, 255, 255, 0)';
-		event.target.style.color = 'black';
 		event.target.blur();
+
 		event.target.removeEventListener('keypress', keyHandler);
 		window.removeEventListener('keydown', preventEscape);
+		event.stopPropagation();
 	}
 
 	terminal.addEventListener('mouseover', activateTerminal);
