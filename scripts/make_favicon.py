7b78e7dfa4a0cab6f927b8f4f1ed869532fe3e7a
Alexandre Vallette <vallette@Mac-5.local>
2014-12-08 18:14:51 +0100

    cleaned + favicon

--- scripts/make_favicon.py
+++ scripts/make_favicon.py
@@ -3,23 +3,42 @@ import matplotlib.colors as mcolors
 import numpy as np
 from matplotlib import animation
 
-iconsize = 16
-
-def animate(nframe):
-	plt.cla()
-	A = np.random.randn(iconsize, iconsize)
-	ax = plt.Axes(fig, [0., 0., 1., 1.])
-	ax.set_axis_off()
-	fig.add_axes(ax)
-	ax.spy(A, precision=0.1)
+iconsize = 32
 
 
-# # plt.show()
-# fig.savefig('figure.png', dpi=1)
-
-
+# for a static fig
 fig = plt.figure(frameon=False)
 fig.set_size_inches(iconsize, iconsize)
+A = np.random.randint(18, size=(iconsize, iconsize))
+def truncate(x):
+	if x < 2:
+		return 1
+	else:
+		return 0
 
-anim = animation.FuncAnimation(fig, animate, frames=40)
-anim.save('../favicon.gif', writer='imagemagick', fps=10);
+truncate = np.vectorize(truncate)
+A = truncate(A)
+ax = plt.Axes(fig, [0., 0., 1., 1.])
+ax.set_axis_off()
+fig.add_axes(ax)
+ax.imshow(A,cmap='Greys', interpolation='nearest')
+fig.savefig('../favicon.png', dpi=1)
+
+
+
+# # for the animation
+
+# def animate(nframe):
+# 	plt.cla()
+# 	A = np.random.randn(iconsize, iconsize)
+# 	ax = plt.Axes(fig, [0., 0., 1., 1.])
+# 	ax.set_axis_off()
+# 	fig.add_axes(ax)
+# 	ax.spy(A, precision=0.99999)
+
+
+# fig = plt.figure(frameon=False)
+# fig.set_size_inches(iconsize, iconsize)
+
+# anim = animation.FuncAnimation(fig, animate, frames=1)
+# anim.save('../favicon.gif', writer='imagemagick', fps=12);
