488985bce68cce12a093db7067752e56ab6646ad
Romain Crestey <rcrestey@gmail.com>
2015-12-28 19:41:17 +0100

    starting to look good

--- js/navigation.js
+++ js/navigation.js
@@ -8,32 +8,62 @@ var offersButton = document.querySelector('#offers-button');
 var openButton = document.querySelector('#open-button');
 var teamButton = document.querySelector('#team-button');
 var contactButton = document.querySelector('#contact-button');
+var tabs = document.getElementsByClassName('tab');
 
 var companies = document.querySelector('#companies');
-var publicInfra = document.querySelector('#public');
+var publicInfra = document.querySelector('#publicInfra');
 var companiesButton = document.querySelector('#companies-button');
 var publicButton = document.querySelector('#public-button');
 
+companies.style.position = 'absolute';
 
 var marginMap = new Map();
 
 marginMap.set(homeButton, 0);
 marginMap.set(offersButton, -80);
-marginMap.set(openButton, -174);
-marginMap.set(teamButton, -264);
-marginMap.set(contactButton, -354);
-
-// offersButton.addEventListener('click', function(){
-// 			console.log('Click on menu');
-// 			main.style.marginTop = -80 + 'px';
-// 		});
+marginMap.set(openButton, -180);
+marginMap.set(teamButton, -280);
+marginMap.set(contactButton, -380);
 
 module.exports = function(){
 	marginMap.forEach(function(margin, button){
 		button.addEventListener('click', function(event){
 			event.preventDefault();
-			console.log('Click on menu');
 			main.style.transform = 'translateY(' + margin + 'vh)';
+
+			tabs[0].className = 'tab';
+			tabs[1].className = 'tab';
+			tabs[2].className = 'tab';
+			tabs[3].className = 'tab';
+			tabs[4].className = 'tab';
+
+			button.classList.toggle('active');
 		});
 	});
+
+	companiesButton.addEventListener('click', function(event){
+		event.preventDefault();
+		main.style.transform = 'translateY(' + marginMap.get(offersButton) + 'vh)';
+		companies.style.transform = 'translateX(-100vw)';
+		publicInfra.style.transform = 'translateX(-100vw)';
+
+		companiesButton.className = 'active';
+		publicButton.className = '';
+
+		tabs[1].className = 'tab active';
+	});
+
+	publicButton.addEventListener('click', function(event){
+		event.preventDefault();
+		main.style.transform = 'translateY(' + marginMap.get(offersButton) + 'vh)';
+		companies.style.transform = 'translateX(0vw)';
+		publicInfra.style.transform = 'translateX(0vw)';
+
+		companiesButton.className = '';
+		publicButton.className = 'active';
+
+		tabs[1].className = 'tab active';
+
+	});
+
 };
